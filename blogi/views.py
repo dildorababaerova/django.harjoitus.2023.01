75d0de5a84689888e6d99694db6b6ec660aa539e
Dildora <48212@edu.raseko.fi>
2023-02-06 15:11:40 +0200

    lisätty

--- blogi/views.py
+++ blogi/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Postaus
 
-# Create your views here.
+
+def postaukset(request):
+    #postaukset = ["eka", "toka"]
+    postaukset = Postaus.objects.all()
+
+    context = {"postaukset": postaukset}
+    return render(
+        request,
+         "blogi/postauslista.html",
+         context,
+         )
+
+
+def nayta_postaus(request, id):
+    return render(request, "blogi/postaus.html")
