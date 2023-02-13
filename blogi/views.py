27d1924b8970fb854d6783cccfdac45259ff5887
Dildora <48212@edu.raseko.fi>
2023-02-13 15:01:29 +0200

    lisätty otsikot

--- blogi/views.py
+++ blogi/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Postaus
+from django.urls import reverse
 
 
 def postaukset(request):
@@ -18,3 +19,31 @@ def nayta_postaus(request, id):
     postaus = Postaus.objects.get(id=id)
     context = {"postaus": postaus}
     return render(request, "blogi/postaus.html", context)
+
+
+def uusi_postaus(request):
+    if request.method == "GET":
+        # 1.Lomake näytetään ensimmäisenä kertaa
+        return render(request, "blogi/uusi_postaus.html")
+    
+    elif request.method == "POST":
+        #2. Käyttäjä täytti ja lahetti lomakkeen
+
+        # Luetaan lomakkeen tiedot POST-datasta
+        otsikko = request.POST["otsikko"]
+        teksti = request.POST['teksti']
+
+        #Luodaan uusi postaus-objekti tietokantaan
+        postaus = Postaus.objects.create(
+            otsikko=otsikko,
+            teksti=teksti,
+
+        )
+        #Muodostetaan URL-osoite luotuun Postaus_objektiin
+        url = reverse('nayta_postaus', args=(postaus.id,))
+
+        #Palautetaan uudelleenohjaus uuden Postaus- objektin URL:iin
+        return redirect(url)
+    
+    
+
--- koodausblogi/urls.py
+++ koodausblogi/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path("", blogi_views.postaukset, name="postauslista"),
     path("postaus/<int:id>", blogi_views.nayta_postaus,
          name="nayta_postaus"),
+    path('uusi/', blogi_views.uusi_postaus, name='uusi_postaus')
 ]
 
+                                                                        
