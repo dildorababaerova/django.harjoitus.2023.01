9e3f0826fbedf2c023f6df8312a26847f8b82488
Dildora <48212@edu.raseko.fi>
2023-03-10 08:54:58 +0200

    lisätty
    lisätty

--- blogi/models.py
+++ blogi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
-    ingressi = models.CharField(max_length=5000, blank=True)
+    ingressi = models.TextField(blank=True)
     teksti = models.TextField()
     kuva = models.ImageField(upload_to="blogi/kuvat", null=True, blank=True)
     luotu = models.DateTimeField(auto_now_add=True)
@@ -14,6 +14,4 @@ class Postaus(models.Model):
     def luo_ingressi(self):
         if self.ingressi:
             return self.ingressi
-        return self.teksti[:50]+ "..."    
-    
-            
+        return self.teksti[:50] + "..."
